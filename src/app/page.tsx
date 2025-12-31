5e0c956eb737c52a4da2d4e8762f979381b611e9
Johnny Gérard <john@jgerard.dev>
2025-12-31 22:59:03 +0100

    feat: add h1 to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,21 @@ import { tw } from "~/utils/tw";
 
 const HomePage: FC = () => (
   <div className="grid min-h-screen place-items-center">
-    <Link
-      href="/studio"
-      className={tw([
-        "inline-flex items-center justify-center rounded-md",
-        "bg-gray-900 px-4 py-2",
-        "text-sm font-medium text-gray-50",
-        "transition-colors hover:bg-gray-800",
-        "outline-offset-2 focus-visible:outline-gray-900/40",
-      ])}
-    >
-      Open Sanity Studio
-    </Link>
+    <div className="flex flex-col items-center gap-6">
+      <h1>Deployment Successful!</h1>
+      <Link
+        href="/studio"
+        className={tw([
+          "inline-flex items-center justify-center rounded-md",
+          "bg-gray-900 px-4 py-2",
+          "text-sm font-medium text-gray-50",
+          "transition-colors hover:bg-gray-800",
+          "outline-offset-2 focus-visible:outline-gray-900/40",
+        ])}
+      >
+        Open Sanity Studio
+      </Link>
+    </div>
   </div>
 );
 
