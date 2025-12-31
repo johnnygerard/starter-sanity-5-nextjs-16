1128185183ae241d547fd90096b1c8349c6dc269
Johnny Gérard <john@jgerard.dev>
2025-12-31 22:04:43 +0100

    refactor: improve src/sanity/env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,20 +1,22 @@
+const assertAndGetVariable = (
+  value: string | undefined,
+  name: string,
+): string => {
+  if (value === undefined)
+    throw new Error(`The environment variable "${name}" is undefined.`);
+
+  return value;
+};
+
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-12-31";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "2025-12-31";
 
-export const dataset = assertValue(
+export const dataset = assertAndGetVariable(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
+  "NEXT_PUBLIC_SANITY_DATASET",
 );
 
-export const projectId = assertValue(
+export const projectId = assertAndGetVariable(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+  "NEXT_PUBLIC_SANITY_PROJECT_ID",
 );
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
-
-  return v;
-}
