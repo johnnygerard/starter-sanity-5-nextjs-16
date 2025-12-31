4eabb1198f6a998bc39e234f3a84761ba500fd0c
Johnny Gérard <john@jgerard.dev>
2025-12-31 21:50:33 +0100

    refactor: clean up sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,13 @@
 /**
- * This configuration file lets you run `$ sanity [command]` in this folder
- * Go to https://www.sanity.io/docs/cli to learn more.
+ * Sanity CLI configuration file
+ * @see https://www.sanity.io/docs/apis-and-sdks/cli
  **/
+import { env } from "node:process";
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-
-export default defineCliConfig({ api: { projectId, dataset } });
+export default defineCliConfig({
+  api: {
+    dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  },
+});
