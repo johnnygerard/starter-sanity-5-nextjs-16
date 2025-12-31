77126baf083cea663024a14213c8c69defce89fa
Johnny Gérard <john@jgerard.dev>
2025-12-31 23:25:54 +0100

    refactor: clean up StudioPage component

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,17 @@
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
+ * https://nextjs.org/docs/app/api-reference/file-conventions/dynamic-routes#optional-catch-all-segments
  *
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
-
 import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
+import type { FC } from "react";
+import config from "~root/sanity.config";
 
 export const dynamic = "force-static";
-
 export { metadata, viewport } from "next-sanity/studio";
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
-}
+const StudioPage: FC = () => <NextStudio config={config} />;
+export default StudioPage;
