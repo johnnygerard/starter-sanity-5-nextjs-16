36b5e44dfff2cea93905a86ab99e1d685110a1ad
Johnny Gérard <john@jgerard.dev>
2025-12-31 22:35:05 +0100

    feat: update homepage with link to Sanity Studio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
 import type { FC } from "react";
+import { tw } from "~/utils/tw";
 
-const HomePage: FC = () => {
-  return (
-    <div className="grid min-h-screen place-items-center">
-      <h1>Deployment successful!</h1>
-    </div>
-  );
-};
+const HomePage: FC = () => (
+  <div className="grid min-h-screen place-items-center">
+    <Link
+      href="/studio"
+      className={tw([
+        "inline-flex items-center justify-center rounded-md",
+        "bg-gray-900 px-4 py-2",
+        "text-sm font-medium text-gray-50",
+        "transition-colors hover:bg-gray-800",
+        "outline-offset-2 focus-visible:outline-gray-900/40",
+      ])}
+    >
+      Open Sanity Studio
+    </Link>
+  </div>
+);
 
 export default HomePage;
