b2af14b6dff2eccbd31cb285d56e771db7a819d5
Johnny Gérard <john@jgerard.dev>
2025-12-31 21:54:48 +0100

    refactor: clean up src/sanity/lib/image.ts

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -2,12 +2,9 @@ import {
   createImageUrlBuilder,
   type SanityImageSource,
 } from "@sanity/image-url";
+import { dataset, projectId } from "~/sanity/env";
 
-import { dataset, projectId } from "../env";
-
-// https://www.sanity.io/docs/image-url
+// https://www.sanity.io/docs/apis-and-sdks/image-urls
 const builder = createImageUrlBuilder({ projectId, dataset });
 
-export const urlFor = (source: SanityImageSource) => {
-  return builder.image(source);
-};
+export const urlFor = (source: SanityImageSource) => builder.image(source);
